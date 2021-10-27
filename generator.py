a25c7011c34695681f92b441c02837ff66495495
RandomGuy090 <randomguy0090@gmail.com>
2021-10-27 14:23:31 +0200

    generating, int conversion bugfix

--- generator.py
+++ generator.py
@@ -5,7 +5,12 @@ import variables as var
 class Generator(object):
 	def generate(self, lenght):
 		ret = ""
-		for i in range(int(lenght)):
+		try:
+			x = int(lenght)
+		except:
+			x = 0
+
+		for i in range(x):
 			ind = random.randrange(len(string.ascii_letters))
 			ret+= string.ascii_letters[ind]
 		return ret
